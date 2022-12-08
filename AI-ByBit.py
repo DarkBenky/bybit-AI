8e1c72c30f7e700c896d172d3f813b6493ecdc4c
DarkBenky <matus.benky@gmail.com>
2022-12-08 20:20:00 +0100

    model fixing

--- AI-ByBit.py
+++ AI-ByBit.py
@@ -43,30 +43,31 @@ def main():
 	x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 	x_test = np.array(x_test)
 	x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+	print(x_train)
+	print("-"*36)
+	print(y_train)
 	
 	model = Sequential()
 	model.add(LSTM(256, input_shape=(x_train.shape[1],1), return_sequences=True))
 	model.add(Dropout(0.2))
 	model.add(LSTM(512, return_sequences=True))
 	model.add(Dropout(0.2))
-	model.add(LSTM(256, return_sequences=True))
+	model.add(LSTM(256 , return_sequences=True))
 	model.add(Dropout(0.2))
-	model.add(LSTM(128, return_sequences=True))
+	model.add(LSTM(128 , return_sequences=True))
 	model.add(Dropout(0.2))
-	model.add(LSTM(64, return_sequences=True))
+	model.add(LSTM(64 , return_sequences=True))
 	model.add(Dropout(0.2))
-	model.add(LSTM(32, return_sequences=True))
-	model.add(Dense(1))
+	model.add(LSTM(32))
+	model.add(Dense(units=1))
 		
 		
 	model.compile(loss="mean_squared_error", optimizer="adam")
-	checkpoint = ModelCheckpoint("model_best.h5", monitor="loss", verbose=1, save_best_only=True, mode="auto")
+	# save model and weights
+	checkpoint = ModelCheckpoint("model.h5", monitor="loss", verbose=1, save_best_only=True, mode="auto")
 	with tf.device("/gpu:0"):
 		model.fit(x_train, y_train, epochs=10000, batch_size=32, validation_data=(x_test, y_test), callbacks=[checkpoint])
-
-	model.save("model.h5")
-	model.save_weights("model_weights.h5")
-		
+	
 	predictions = model.predict(x_test)
 	predictions = scalar.inverse_transform(predictions)
 	y_test = scalar.inverse_transform(y_test)
@@ -75,19 +76,37 @@ def main():
 		
  
 
-def predict_price(model, data, scalar_data):
+def predict_price(model_path, y_test, x_test):
+	"""Predicts the price of the next candle"""
 	scalar = MinMaxScaler(feature_range=(0,1))
-	data = scalar.fit_transform(scalar_data)
-	#load tensorflow model
-	model = load_model(model)
-	#load weights
-	model.load_weights("model_weights.h5")
-	#predict price
-	prediction = model.predict(data)
-	prediction = scalar.inverse_transform(prediction)
-	return prediction
+	scalar.fit_transform(y_test)
+	model = load_model(model_path)
+	predictions = model.predict(x_test)
+	predictions = scalar.inverse_transform(predictions)
+	y_test = scalar.inverse_transform(y_test)
+	print("ERROR: ", accuracy(predictions, y_test))
+	return predictions
 
+def prepare_data():
+	df = goose.load_data_csv("formatted_data.csv")
+
+	y = goose.select_data_at_index(df, 0)
+	y = pd.DataFrame(y).astype(float)
+
+	x = goose.pop_all_data_at_index(df, 0)
+	x = pd.DataFrame(x).astype(float)
+
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+	#reshape data
+	x_test = np.array(x_test)
+	x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+	return x_test , y_test
 
 if __name__ == "__main__":
 	main()
+	x_test , y_test = prepare_data()
+	predict_price("model.h5", y_test, x_test)
+	
+
+
 	
